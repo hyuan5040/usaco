fc0402e15718243bf7ca49a8146bf9a9995d0ad2
hyuan71 <yuan_henry@yahoo.com>
2019-11-25 10:47:13 -0800

    pass all the test cases 4 problem uno

--- 2018-12/Problem1/main.cpp
+++ 2018-12/Problem1/main.cpp
@@ -5,19 +5,19 @@ int main () {
     cin >> c1 >> m1 >> c2 >> m2 >> c3 >> m3;
     for (int i = 0; i < 100; i ++) {
         if (i % 3 == 0) {
-            while (m1 >= 0 && m2 < c2) {
+            while (m1 > 0 && m2 < c2) {
                 m1 --;
                 m2 ++;
             }
         }
         else if (i % 3 == 1) {
-            while (m2 >= 0 && m3 < c3) {
+            while (m2 > 0 && m3 < c3) {
                 m2 --;
                 m3 ++;
             }
         }
         else {
-            while (m3 >= 0 && m1 < c1) {
+            while (m3 > 0 && m1 < c1) {
                 m3 --;
                 m1 ++;
             }
