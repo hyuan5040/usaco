8814bf2e785356acaa32024fd34203fe4ae50df4
hyuan71 <yuan_henry@yahoo.com>
2019-11-25 22:46:21 -0800

    bronze 2019-01 problems

---
+++ bronze/2019-01/Problem2.cpp
@@ -0,0 +1,9 @@
+#include <cstdio>
+#include <cstring>
+#include <iostream>
+using namespace std;
+int n, s[105], b[105], t[105];
+int bk[1005];
+int main () {
+
+ }
---
+++ bronze/2019-01/Problem3.cpp
@@ -0,0 +1,9 @@
+#include <bits/stdc++.h>
+using namespace std;
+multiset<int> S[2]; 
+int pos[2001];
+int numOutcomes;
+
+int main() {
+
+}
