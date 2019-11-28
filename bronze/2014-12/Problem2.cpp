5fbb73523050c793edfc9ada1f78852a134a548e
hyuan71 <yuan_henry@yahoo.com>
2019-11-28 14:45:30 -0800

    the 2018-1 thingys

--- bronze/2014-12/Problem2.cpp
+++ bronze/2014-12/Problem2.cpp
@@ -1,9 +1,38 @@
-#include <bits/stdc++.h>
+#include <cstdio>
+#include <vector>
+#include <fstream>
+#define MP make_pair
+#define PB push_back
+#define X first
+#define Y second
 using namespace std;
-struct lifeguard {
-    int start;
-    int end;
-};
+typedef long long ll;
+typedef pair<ll, ll> pll;
 int main() {
-
+  ifstream fin(crosswords.in);
+  ofstream fout(crosswords.out);
+  long long R, C;
+  fin >> R >> C;
+  vector<vector<char> > G(R, vector<char>(C, ' '));
+  for(long long r = 0; r < R; r ++) {
+    char buf[100];
+    scanf("%s", buf);
+    for(ll c = 0; c < C; c ++) {
+      G[r][c] = buf[c];
+    }
+  }
+  vector<pair<long long, long long> A;
+  for(long long r = 0; r < R; r ++) {
+    for(ll c = 0; c < C; c ++) {
+      bool horizontal = (c + 2 < C && G[r][c ] == '.' && G[r][c + 1] == '.' && G[r][c + 2] == '.' && (c == 0 || G[r][c - 1] == '#'));
+      bool vertical = (r + 2 < R && G[r][c] == '.' && G[r + 1][c]== '.' && G[r + 2][c] == '.' && (r == 0 || G[r - 1][c] == '#'));
+      if(horizontal || vertical) {
+        A.PB(MP(r + 1, c + 1));
+      }
+    }
+  }
+  fout << A.size << endl;
+  for(pll clue : A) {
+    fout << clue.first << " " << clue.second << endl;
+  }
 }
