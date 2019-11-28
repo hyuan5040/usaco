4e9ac572e67c75b45e8d6734b26bbefbfab7192c
hyuan71 <yuan_henry@yahoo.com>
2019-11-27 16:49:04 -0800

    the 2018-1 thingys

--- bronze/2018-1/Problem2.cpp
+++ bronze/2018-1/Problem2.cpp
@@ -1,28 +1,29 @@
 #include <bits/stdc++.h>
 using namespace std;
 struct lifeguard {
-    int start, end;
-} lifeguard;
+    int start;
+    int end;
+};
 int main() {
     int n;
     cin >> n;
     lifeguard lifeguards[n];
     int max_time = 0;
     for (int i = 0; i < n; i ++) {
-        cin >> lifeguards.start >> lifeguards.end;
+        cin >> lifeguards[i].start >> lifeguards[i].end;
     }
     for (int i = 0; i < n; i ++) {
         int time[1000] = {};
+        int temp = 0;
         for (int j = 0; j < n; j ++) {
-            int temp = 0;
             if (j == i) {
                 break;
             }
             else {
-                for (int k = lifeguards.start; k < lifeguards.end; k ++) {
+                for (int k = lifeguards[j].start; k < lifeguards[j].end; k ++) {
                     if (time[k] == 0) {
                         temp ++;
-                        time[k] == 1;`
+                        time[k] == 1;
                     }
                 }
             }
