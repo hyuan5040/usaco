b9844e8ed0e2c8b85624aae92fa36555797b1111
hyuan71 <yuan_henry@yahoo.com>
2019-11-29 16:23:18 -0800

    2014-12

--- bronze/2014-12/Problem1.cpp
+++ bronze/2014-12/Problem1.cpp
@@ -1,43 +1,39 @@
 #include <iostream>
-#include <cmath>
 #include <algorithm>
+#include <utility>
+#include <vector>
+#include <fstream>
 using namespace std;
-int calc_md(int x1, int y1, int x2, int y2) {
-    return abs(x1 - x2) + abs(y1 - y2);
+int calc_md(pair<int, int> one, pair<int, int> two) {
+    return abs(one.first - two.first) + abs(one.second - two.second);
 }
-struct checkpoint {
-    int x;
-    int y;
-};
+int total (vector<pair<int, int>> points, int n) {
+    int total = 0;
+    for (int i = 1; i < n; i ++) {
+        total += calc_md(points[i], points[i - 1]);
+    }
+    return total;
+}
 int main () {
+    ifstream fin ("marathon.in");
+    ofstream fout ("marathon.out");
     int n;
-    cin >> n;
-    checkpoint checkpoints[n];
-    int time_saved[n] = {};
+    fin >> n;
+    vector <pair<int, int>> points;
     for (int i = 0; i < n; i ++) {
-        cin >> checkpoints[i].x >> checkpoints[i].y;
+        pair <int, int> temp;
+        fin >> temp.first >> temp.second;
+        points.push_back(temp);
     }
+    int totall = total(points, n);
+    //cout << "total1 is " << totall << endl;
+    int least = 2147483647;
     for (int i = 1; i < n - 1; i ++) {
-        int x1 = checkpoints[i - 1].x;
-        int x2 = checkpoints[i].x;
-        int x3 = checkpoints[i + 1].x;
-        int y1 = checkpoints[i - 1].y;
-        int y2 = checkpoints[i].y;
-        int y3 = checkpoints[i + 1].y;
-        for (int j = 1; j < n - 1; j ++) {
-            time_saved[i] += calc_md(checkpoints[j].x, checkpoints[j].y, checkpoints[j + 1].x, checkpoints[j + 1].y);
-        }
-        time_saved[i] += calc_md(x1, y1, x3, y3);
-        time_saved[i] -= calc_md(x1, y1, x2, y2);
-        time_saved[i] -= calc_md(x2, y2, x3, y3);
-    }
-    int max = 0;
-    int position;
-    for (int i = 1; i < n - 1; i ++) {
-        if (time_saved[i] > max) {
-            max = time_saved[i];
-            position = i;
+        //int temp = totall - ( calc_md(points[i - 1], points[i]) - calc_md(points[i + 1], points[i] -  calc_md(points[i - 1], points[i + 1]))
+        int temp = totall + calc_md(points[i - 1], points[i + 1]) - calc_md(points[i - 1], points[i]) - calc_md(points[i + 1], points[i]);
+        if (temp < least) {
+            least = temp;
         }
     }
-    cout << time_saved[position];
+    fout << least;
 }
