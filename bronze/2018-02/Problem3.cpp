6e515036a39c9d5e71a3c66a80e06a3265ecc73f
hyuan71 <yuan_henry@yahoo.com>
2019-12-07 10:15:45 -0800

    bronze\2018-12 more problems

---
+++ bronze/2018-02/Problem3.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+#include <algorithm>
+#include <utility>
+#include <vector>
+#include <fstream>
+using namespace std;
+
+int main (int argc, char *argv[]) {
+    string inputFile = "taming.in";
+    string outputFile = "taming.out";
+    if (argc >2) {
+        inputFile = argv[1];
+        outputFile = argv[2];
+    }
+    ifstream fin (inputFile);
+    ofstream fout (outputFile);
+    fout << "42";
+}
